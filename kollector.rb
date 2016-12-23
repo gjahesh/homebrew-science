8be6713ae6a674e8b656857bfa3cc506a16e7a2d
Golnaz Jahesh <gjahesh@bcgsc.ca>
2016-12-23 13:34:25 -0800

    Fix test do block

--- kollector.rb
+++ kollector.rb
@@ -1,11 +1,9 @@
-
 class Kollector < Formula
   desc "Targeted de novo Gene Assembly"
   homepage "https://github.com/bcgsc/kollector"
   url "https://github.com/bcgsc/kollector/archive/V1.0.tar.gz"
   version "1.0.0"
   sha256 "76dc3e03398ade0ae39b241943ce7dabcf65c09e3b30180726bfea574c462d15"
-  # tag "bioinformatics"
 
   depends_on "abyss" => :build
   depends_on "biobloomtools" => :build
@@ -19,11 +17,12 @@ class Kollector < Formula
     bin.install "./bin/kollector_multiple.sh"
     doc.install "README.md"
     doc.install "LICENSE.txt"
+    prefix.install Dir["*"]
     prefix.install "test"
     prefix.install "data"
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/kollector.sh", 255)
+    system "#{bin}/kollector.sh", "-h"
   end
 end
